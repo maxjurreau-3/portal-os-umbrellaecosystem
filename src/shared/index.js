4e4f6b088bfc8e4aab8b9bf843e6561005016194
maxjurreau-3 <maxjurreau@gmail.com>
2026-07-31 22:58:48 -0400

    Create index.js to export shared utilities

---
+++ src/shared/index.js
@@ -0,0 +1,9 @@
+// ---------------------------------------------------------------------------
+// Shared Utilities Runtime Integration — Portal‑OS‑v3
+// ---------------------------------------------------------------------------
+
+export * from "./math.js";
+export * from "./time.js";
+export * from "./id.js";
+export * from "./os-helpers.js";
+export * from "./engine-utils.js";
